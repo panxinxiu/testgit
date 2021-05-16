ddbdc4aa1f0cb8302d243cfd1898a905a53e0ff4
panxinxiu <2811394260@qq.com>
2021-05-16 09:52:52 +0800

    我修改了TestGitDemo2,将项目提交并推送到远程仓库

--- src/com/git/TestGitDemo2.java
+++ src/com/git/TestGitDemo2.java
@@ -3,5 +3,6 @@ package com.git;
 public class TestGitDemo2 {
     public static void main(String[] args) {
         System.out.println("这是TestGitDemo2");
+        System.out.println("修改了");
     }
 }
